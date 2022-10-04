d86384f025833e903c576be0b6871a16f450c9ae
Ziyodulla13 <ibaydullayevziyodulla9@gmail.com>
2022-10-04 22:37:43 +0500

    user crud

---
+++ src/main/java/uz/pdp/user/service/UserService.java
@@ -0,0 +1,41 @@
+package uz.pdp.user.service;
+
+import com.example.demo.user.model.User;
+import com.example.demo.user.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepo userRepo;
+
+    public void save(User user) {
+        userRepo.save(user);
+    }
+
+    public Page<User> getAllUsers(int page, int size) {
+        return userRepo.findAll(PageRequest.of(page-1,size));
+    }
+
+    public boolean delete(int id) {
+        try {
+            userRepo.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public User getUserById(int id) {
+        Optional<User> userById = userRepo.findById(id);
+        return userById.get();
+    }
+
+}
+
